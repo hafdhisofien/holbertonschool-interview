b1bd6357d044cc9977d921466615c43bf9f3080e
hafdhisofien <1114@holbertonschool.com>
2020-12-10 10:53:07 +0000

    locked

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    Method to check if all boxes can be opened 
+    """
+    seen = [False] * len(boxes)
+    seen[0] = True
+    stack = [0]
+       
+    while stack:  
+        node = stack.pop()  
+        for nei in boxes[node]:  
+            if not seen[nei]:  
+                seen[nei] = True  
+                stack.append(nei)  
+    return all(seen)

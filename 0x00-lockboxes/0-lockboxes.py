9f1030751f36e08ed7b815df49bc12af13847161
hafdhisofien <1114@holbertonschool.com>
2020-12-10 10:58:20 +0000

    ol

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,10 +3,13 @@ def canUnlockAll(boxes):
     """
     Method to check if all boxes can be opened 
     """
+    if not boxes or len(boxes) ==0:
+        return False
+    
     seen = [False] * len(boxes)
     seen[0] = True
     stack = [0]
-       
+    
     while stack:  
         node = stack.pop()  
         for nei in boxes[node]:  

43d163b1d31e3f9c9b0e8fbd0857f5f7f2da29c7
hafdhisofien <1114@holbertonschool.com>
2020-12-23 17:18:49 +0000

    push all

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,10 +1,10 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stddef.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
-#include <unistd.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -19,10 +19,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s binary_tree_t;
-void binary_tree_print(const binary_tree_t *);
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */

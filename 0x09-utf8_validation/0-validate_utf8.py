79fd3188e5a742c3629131893d0b0bd31afd6ae5
hafdhisofien <1114@holbertonschool.com>
2021-02-18 17:49:28 +0000

    pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
 UTF-8 validation
 """
 
 
 def validUTF8(data):
-    """ [validUTF8]: method that determines if a given data set represents a valid UTF-8 encoding
+    """ [validUTF8]: method that determines if a given data
+        set represents a valid UTF-8 encoding
     """
     try:
         string = [n & 255 for n in data]

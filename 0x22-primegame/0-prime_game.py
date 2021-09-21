535d814b34a68d3bcbdbe2d46b4cf64b797d8f3e
hafdhisofien <1114@holbertonschool.com>
2021-09-21 11:31:41 +0100

    prime game

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""[Prime Game]
+"""
+
+
+def isWinner(x, nums):
+    """[Prime Game]
+    Args:
+        x is the number of rounds
+        nums is an array of n
+    Returns:
+        name of the player that won the most rounds
+    """
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    arr = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not arr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            arr[j] = False
+    arr[0] = arr[1] = False
+    counter = 0
+    for i in range(len(arr)):
+        if arr[i]:
+            counter += 1
+        arr[i] = counter
+    player = 0
+    for n in nums:
+        player += arr[n] % 2 == 1
+    if player * 2 == len(nums):
+        return None
+    if player * 2 <= len(nums):
+        return "Ben"
+    return "Maria"
---
+++ 0x22-primegame/README.md
@@ -0,0 +1,1 @@
+__ 0x22-primegame __
